f2bf1e995b5c9b5759ca76718fd23d3510106299
João Vitor de Oliveira <155332038+Jvsvic@users.noreply.github.com>
2025-01-29 11:31:52 -0300

    Add files via upload

---
+++ screenmatch-spring/src/main/java/br/com/alura/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchSpringApplication.class, args);
+	}
+
+}
